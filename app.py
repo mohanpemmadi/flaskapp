3d88ba3d9fffd9ac4bd2eb2d1a1904f66ea14713
pitchaiah554 <pitchaiah.python@gmail.com>
2022-09-22 07:43:52 +0530

    flask class 5

--- app.py
+++ app.py
@@ -33,9 +33,50 @@ class AddEmployee(Resource):
         emp_data = {'id':emp.id,'name':emp.name,'age':emp.age,'location':emp.location,'salary':emp.salary}
         return {'message':f'Employee {emp.name} added', 'data':emp_data, 'status':200}
 
+class ListEmployees(Resource):
+    def get(self):
+        emp_data = Employee.query.all() # query set
+        # print('emp_data : ', emp_data)
+        res_list = []
+        for emp in emp_data:
+            res_list.append({'id':emp.id,'name':emp.name,'age':emp.age,'location':emp.location,'salary':emp.salary})
+        return {'data':res_list}
+
+class UpdateEmployees(Resource):
+    def put(self, id):
+        req_body = request.get_json()
+        emp = Employee.query.get(id)
+        if emp is not None:
+            emp.name = req_body['name']
+            emp.age = req_body['age']
+            emp.location = req_body['location']
+            emp.salary = req_body['salary']
+            db.session.commit()
+            return {'data':{'id':emp.id,'name':emp.name,'age':emp.age,'location':emp.location,'salary':emp.salary}}
+        else:
+            return {'message': f'employee id {id} does not exists'}
+
+class DeleteEmployees(Resource):
+    def delete(self, id):
+        emp = Employee.query.get(id)
+        if emp is not None:
+            db.session.delete(emp)
+            db.session.commit()
+            return {'message': f'Employee {emp.name} deleted successfully'}
+        else:
+            return {'message': f'Employee id {id} does not exists'}
+
+class GetEmployee(Resource):
+    def get(self, id):
+        emp = Employee.query.get(id)
+        return {'data':{'id':emp.id,'name':emp.name,'age':emp.age,'salary':emp.salary,'location':emp.location}}
+
 
 api.add_resource(AddEmployee, '/add_new_employee')
-api.add_resource(AddEmployee, '/get_all_employees')
+api.add_resource(ListEmployees, '/list_employees')
+api.add_resource(UpdateEmployees, '/update_emp/<int:id>')
+api.add_resource(DeleteEmployees, '/delete_emp/<int:id>')
+api.add_resource(GetEmployee, '/get_employee/<int:id>')
 
 if __name__ == '__main__':
     app.run(debug=True)
